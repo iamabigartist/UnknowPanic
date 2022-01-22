037a118c1af336139c9b36f777d429814c6b7332
iamabigartist <1548016008@qq.com>
2022-01-22 16:03:56 +0800

    init

---
+++ Assets/UnknownPanic/Scripts/Tests/Editor/TestColorConvert.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnknownPanic.Utils;
+namespace UnknownPanic.Tests
+{
+    public class TestColorConvert : EditorWindow
+    {
+        [MenuItem( "Tests/TestColorConvert" )]
+        static void ShowWindow()
+        {
+            var window = GetWindow<TestColorConvert>();
+            window.titleContent = new GUIContent( "TestColorConvert" );
+            window.Show();
+        }
+
+        Color m_color;
+        string color_html_hex;
+        const string plain_text = "I have a ▙a▓▟░a";
+        string rich_text;
+        GUIStyle label_style;
+        void OnEnable()
+        {
+            label_style = new GUIStyle()
+            {
+                richText = true,
+            };
+        }
+        void OnGUI()
+        {
+            m_color = EditorGUILayout.ColorField( m_color );
+            color_html_hex = ColorUtility.ToHtmlStringRGBA( m_color );
+            rich_text = plain_text.AddColorToText( m_color, 9, 6 );
+            EditorGUILayout.LabelField( $"color: {color_html_hex}" );
+            GUILayout.Label( $"rich text: {rich_text}", label_style );
+        }
+
+    }
+}
---
+++ Assets/UnknownPanic/Scripts/Utils/RichTextUtils.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+namespace UnknownPanic.Utils
+{
+    public static class RichTextUtils
+    {
+        public const string R_COLOR_TAG = "</color>";
+        public static string L_COLOR_TAG(Color color)
+        {
+            var color_html_hex = ColorUtility.ToHtmlStringRGBA( color );
+            return $"<color=#{color_html_hex}>";
+        }
+
+        /// <summary>
+        ///     Remove part of the string and replace with new one.
+        /// </summary>
+        public static string ReplacePosition(this string ori_str, string replacement, int replace_start, int replace_length)
+        {
+            var result_text = ori_str.Remove( replace_start, replace_length );
+            result_text = result_text.Insert( replace_start, replacement );
+            return result_text;
+        }
+
+        public static string AddColorToText(this string plain_text, Color color, int start, int length)
+        {
+            var colored = plain_text.Substring( start, length );
+            colored = L_COLOR_TAG( color ) + colored + R_COLOR_TAG;
+            var rich_text = plain_text.ReplacePosition( colored, start, length );
+            return rich_text;
+        }
+    }
+}
