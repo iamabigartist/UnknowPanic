deef96acf7a35f71eef9facaa8062562faaa0149
iamabigartist <1548016008@qq.com>
2022-01-29 14:28:20 +0800

    clean add rich text

---
+++ Assets/Project/PlayableRichText/Scripts/RichTextObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+namespace PlayableRichText.Scripts
+{
+    public class RichTextObject : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
